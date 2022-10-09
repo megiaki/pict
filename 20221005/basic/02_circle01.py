b397eb086254cc37420fb48f48b4a93b157cb9a7
moegiAki <97770639+megiaki@users.noreply.github.com>
2022-10-09 14:26:07 +0900

    丸パターン追加

---
+++ 20221005/basic/02_circle01.py
@@ -0,0 +1,16 @@
+import turtle
+
+# ポインターの形設定
+turtle.shape('classic')
+
+#色の設定
+turtle.color('green')
+
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+
+# 円を描く
+turtle.circle(100)
+
+turtle.done()
